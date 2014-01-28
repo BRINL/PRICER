0011149cd672213ad043a72d6bb259f1e0b22270
Loïc Brin <loicbrin@MacBook-Air-de-Loic.local>
2014-01-28 13:50:23 +0100

    Initial Commit

---
+++ PRICER/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  PRICER
+//
+//  Created by Loïc Brin on 28/01/14.
+//  Copyright (c) 2014 Aymeric BELLON / Loïc BRIN. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
