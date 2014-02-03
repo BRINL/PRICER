585986808919922bc6204ba3f207867391645299
BRINL <loic.brin@hec.edu>
2014-02-03 12:11:34 +0100

    Grande modif'

---
+++ Genealeatoire.h
@@ -0,0 +1,11 @@
+// Genealeatoire //
+
+#ifndef __PRICER__Genealeatoire__
+#define __PRICER__Genealeatoire__
+
+#include <iostream>
+
+double GetOneGaussianBySummation();
+double GetOneGaussianByBoxMuller();
+
+#endif /* defined(__PRICER__Genealeatoire__) */
