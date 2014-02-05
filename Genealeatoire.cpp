3acec2f805f469c710498255b310122c7e15ebf2
BRINL <loic.brin@hec.edu>
2014-02-05 21:30:54 +0100

    Après le train

--- Genealeatoire.cpp
+++ Genealeatoire.cpp
@@ -1,37 +1,34 @@
 //
 //  Genealeatoire.cpp
 //  Le but est de générer des observations de variables aléatoires suivant des lois définies dans des sous classes ///
+// Loïc : Ce n'est pas une classe, juste une externalisation d'une fonction. Il faudrait peut-être créer une classe genealeatoire et deux filles : normale / poisson. On peut jeter ces techniques et en créer des nouvelles//
+
+
+
 
 #include "Genealeatoire.h"
 #include <cstdlib>
 #include <cmath>
 
-#if !defined(_MSC_VER)
 using namespace std;
-#endif
 
-double GetOneGaussianBySummation()
+Normale :: Normale(double mu, double sigma2) : m_mu(mu), m_sigma2(sigma2) // constructeur Normale
 {
-    double result=0;
-    for (unsigned long j=0; j<12; j++)
-        result += rand()/static_cast<double>(RAND_MAX);
-        result-=6.0;
-        return result;
 }
-    
-double GetOneGaussianByBoxMuller()
+
+double Normale:: gNormale() const
 {
-    double result;
-    double x;
-    double y;
-    double sizeSquared;
-    do
-    {
-        x=2.0*rand()/static_cast<double>(RAND_MAX)-1;
-        y=2.0*rand()/static_cast<double>(RAND_MAX)-1;
-        sizeSquared=x*x+y*y;
-    }
-    while (sizeSquared>=1.0);
-    result = x*sqrt(-2*log(sizeSquared)/sizeSquared);
-    return result;
+    // ICI simulation d'une loi normale (m_mu, m_sigma2)
+    return m_mu+m_sigma2 ;
 }
+
+Poisson :: Poisson(double lambda) : m_lambda(lambda) // Constructeur Poisson
+{
+}
+
+double Poisson::gPoisson() const
+{
+    // ICI simulation d'une loi de poisson(m_lambda)
+    return 0;
+}
+
